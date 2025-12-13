6e59adf912862983d1d191c015b6b00377a4a655
Ange Arsene <nkenmandenga@gmail.com>
2025-12-13 09:42:49 +0000

    feat: add array_until function as well as a test method in FunctionsTest class to test that  Array until function fetches items in array until key and another one to test that  Array until function throws exception if key does not exists

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,21 @@
 <?php
 
-function example_function(): string
-{
-    return 'Hello, World!';
+if (! function_exists('example_function')) {
+    function example_function(): string
+    {
+        return 'Hello, World!';
+    }
+}
+
+if (! function_exists('array_until')) {
+    function array_until(mixed $stop, array $arr): array
+    {
+        $index = array_search($stop, $arr);
+
+        if ($index === false) {
+            throw new InvalidArgumentException('key does not exists in array');
+        }
+
+        return array_slice($arr, 0, $index);
+    }
 }
--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -6,6 +6,7 @@ namespace Angearsene\LaravelTesting\Tests;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use function array_until;
 use function example_function;
 
 use PHPUnit\Framework\TestCase;
@@ -16,5 +17,25 @@ final class FunctionsTest extends TestCase
     {
         $this->assertTrue(function_exists('example_function'));
         $this->assertEquals('Hello, World!', example_function());
+    }
+
+    public function test_array_until_function_fetches_items_in_array_until_key(): void
+    {
+        $names = ['nken', 'mandeng', 'ange', 'arsene'];
+
+        $this->assertTrue(function_exists('array_until'));
+        $this->assertEquals(array_until('ange', $names), ['nken', 'mandeng']);
+    }
+
+    public function test_array_until_function_throws_exception_if_key_does_not_exists(): void
+    {
+        $names = ['nyeck', 'mandeng', 'hubert', 'stephan'];
+
+        $this->assertTrue(function_exists('array_until'));
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('key does not exists in array');
+
+        array_until('ange', $names);
     }
 }
